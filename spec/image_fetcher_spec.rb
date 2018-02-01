66b221bcf14efc20981d4b4a830f78ca9609e69b
Vahdet Pervan <veeuoh@hotmail.co.uk>
2018-02-01 16:36:32 +0000

    Spec tidy up

--- spec/image_fetcher_spec.rb
+++ spec/image_fetcher_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe ImageFetcher do
 	before(:all) do
 		@images_txt_file = File.join( File.dirname(__FILE__), 'fixtures/images.txt' )
 		@images_with_some_bad_urls = File.join( File.dirname(__FILE__), 'fixtures/images_with_some_bad_urls.txt' )
+		@directory_store_files = File.join( File.dirname(__FILE__), 'files/')
 	end
 
 	after(:each) do
@@ -9,12 +10,12 @@ RSpec.describe ImageFetcher do
 	end
 
   it "Stores the images successfully" do
-  	ImageFetcher.get_all_images(@images_txt_file, File.join( File.dirname(__FILE__), 'files/'))
+  	ImageFetcher.get_all_images(@images_txt_file, @directory_store_files)
   	expect(Helpers.image_count).to eq(8)
   end
 
   it "Stores only some images when some URLs are not valid" do
-  	ImageFetcher.get_all_images(@images_with_some_bad_urls, File.join( File.dirname(__FILE__), 'files/'))
+  	ImageFetcher.get_all_images(@images_with_some_bad_urls, @directory_store_files)
   	expect(Helpers.image_count).not_to eq(8)
   end
 
